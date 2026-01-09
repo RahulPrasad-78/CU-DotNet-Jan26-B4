96f512cab9de741f823147fba4b8636826423207
Rahul Prasad <your-email@example.com>
2026-01-09 14:55:26 +0530

    Added Day 4 TrainingPortal project

---
+++ Assignments/Day_4/TrainingPortal/Controllers/CourseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingPortal.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Topic()
+        {
+            return View();
+        }
+    }
+}
