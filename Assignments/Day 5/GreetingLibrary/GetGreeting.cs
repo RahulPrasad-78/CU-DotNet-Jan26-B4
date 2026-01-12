c44a07c58b5e3546e58ae600ee3562dc03a5ac69
Rahul Prasad <your-email@example.com>
2026-01-12 12:35:08 +0530

    Day 5 - Greeting Library

---
+++ Assignments/Day 5/GreetingLibrary/GetGreeting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingLibrary
+{
+    public class GreetingHelper
+    {
+        public static string GetGreeting(string name)
+        {
+            if(name == null || name == "" || name == " ")
+            {
+                return "Hello Guest";
+            }
+
+            return $"Hello {name}";
+        }
+    }
+}
