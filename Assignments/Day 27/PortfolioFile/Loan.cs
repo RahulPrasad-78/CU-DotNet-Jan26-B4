2c6b0bf165211c775c379d97f066f5d48d235ddc
Rahul Prasad <your-email@example.com>
2026-02-06 16:13:24 +0530

    day 27 Complete

---
+++ Assignments/Day 27/PortfolioFile/Loan.cs
@@ -0,0 +1,93 @@
+﻿namespace PortfolioFile
+{
+
+    internal class Loan
+    {
+        public string ClientName { get; set; }
+        public double Principal { get; set; }
+        public double InterestRate { get; set; }
+        //public Loan()
+        //{
+        //    ClientName = string.Empty;
+        //    Principal = 0;
+        //    InterestRate = 0;
+        //}
+        //public Loan(string name, double p, double ir)
+        //{
+        //    ClientName = name;
+        //    Principal = p;
+        //    InterestRate = ir;
+        //}
+        public override string ToString()
+        {
+            string risk = string.Empty;
+            double total = Principal * (InterestRate / 100);
+            if (InterestRate > 10) risk = "High";
+            else if (InterestRate < 5) risk = "Low";
+            else risk = "Medium";
+            return $"|{ClientName,-15} | ${Principal,-10} | ${total,-10:f2} | {risk,-7} |";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                Console.WriteLine("\nWant to Enter data or stop");
+                string input = Console.ReadLine();
+                if (input == "stop") break;
+
+                Console.Write("Enter the name : ");
+                string name = Console.ReadLine();
+                Console.Write("Enter the Principal : ");
+                double p = double.Parse(Console.ReadLine());
+                Console.Write("Enter the Intreset Rate : ");
+                double rate = double.Parse(Console.ReadLine());
+                string data = name + "," + p + "," + rate;
+                list.Add(data);
+            }
+
+            string path = @"../../../Loan.csv";
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                foreach (var item in list)
+                {
+                    //Console.WriteLine(item);
+                    sw.WriteLine(item);
+                }
+            }
+            //sw.Close();
+
+            //string path1 = @"../../../Loan.csv";
+            using StreamReader sr = new StreamReader(path);
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine($"|{"Name",-15} | {"Principal",-10}  | {"Intreset",-10:f2}  | {"Risk",-7} |");
+            Console.WriteLine("-------------------------------------------------------");
+            while (true)
+            {
+                string data = sr.ReadLine();
+                if (data == null) break;
+                Loan l;
+                try
+                {
+                    string[] arr = data.Split(',');
+
+                    l = new Loan
+                    {
+                        ClientName = arr[0],
+                        Principal = double.Parse(arr[1]),
+                        InterestRate = double.Parse(arr[2])
+                    };
+                    Console.WriteLine(l);
+                }
+                catch
+                {
+                    Console.WriteLine("Unexpexted error");
+                }
+            }
+        }
+    }
+}
---
+++ Projects/WordGuess/WordGuess.cs
@@ -0,0 +1,112 @@
+﻿namespace WordGuess
+{
+    internal class WordGuess
+    {
+        public static string[] words = {
+                "planet", "silver", "forest", "battle", "charge",
+                "stream", "random", "memory", "vision", "thread",
+                "dragon", "border", "friend", "mirror", "stable",
+                "object", "screen", "method", "symbol", "player",
+                "window", "future", "danger", "castle", "rocket",
+                "island", "shadow", "hunter", "bridge", "camera"
+        };
+
+        public static void Validations(char input)
+        {
+            
+        }
+
+        public static void Display(char[] ans, int lives, HashSet<char> flag)
+        {
+            Console.WriteLine("Word :  " + string.Join(' ', ans));
+            Console.WriteLine("Lives Left :  " + lives);
+            Console.Write("Guessed :  ");
+            foreach (var item in flag)
+            {
+                Console.Write((char)item + " ");
+            }
+            Console.WriteLine();
+
+        }
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            
+            //Word Selection
+            int idx = rnd.Next(words.Length);
+            string word = words[idx];
+            word = word.ToUpper();
+
+            //Flag
+            HashSet<char> flag = new HashSet<char>();
+
+            //User Inoputs
+            char input=' ';
+            char[] ans = new char[word.Length];
+            Array.Fill(ans, '_');
+
+            //Other Variables
+            int lives = 6;
+            int count = 0;
+
+            Console.WriteLine(word);
+            //Console.WriteLine(string.Join(" ", ans));
+            while (lives > 0 && count!=word.Length)
+            {
+                Console.WriteLine();
+                Display(ans, lives, flag);
+                try
+                {
+                    Console.Write("Enter Word : ");
+                    input = Console.ReadLine()[0];
+                    input = Char.ToUpper(input);
+                    if (input < 'A' || input > 'Z')
+                    {
+                        Console.WriteLine("Enter Value Again");
+                        continue;
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Enter Right Character");
+                    continue;
+                }
+
+                if (flag.Contains(input))
+                {
+                    Console.WriteLine("You Already Used The word");
+                    //Display(ans, lives, flag);
+                    continue;    
+                }
+
+                else
+                    flag.Add(input);
+
+                bool check = false;
+                for(int i =0; i<word.Length; i++)
+                {
+                    if (word[i] == input)
+                    {
+                        check = true;
+                        ans[i] = word[i];
+                        //Display(ans, lives, flag);
+                        count++;
+                    }
+                }
+                if (!check)
+                {
+                    lives--;
+                    Console.WriteLine("Wrong Value");
+                    //Display(ans, lives, flag);
+                }
+                
+            }
+
+            if(lives > 0 && count == word.Length )
+                Console.WriteLine("You Won");
+            else
+                Console.WriteLine("You Lost");
+
+        }
+    }
+}
