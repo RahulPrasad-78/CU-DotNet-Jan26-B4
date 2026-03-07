95dc9552d3205bee9c714164cc10be5712aed5e5
Rahul Prasad <your-email@example.com>
2026-03-07 10:22:32 +0530

    Formatting and uploading Exercise

---
+++ Assignments/Day 47/Cargo/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace Cargo
+{
+    class Item
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Category { get; set; }
+        public Item(string name, double weight, string category)
+        {
+            Name = name;
+            Weight = weight;
+            Category = category;
+        }
+    }
+    class Container
+    {
+        public string ContainerID { get; set; }
+        public List<Item> Items { get; set; }
+        public Container(string containerID, List<Item> items)
+        {
+            ContainerID = containerID;
+            Items = items;
+        }
+    }
+    internal class Program
+    {
+        static List<string> FindHeavyContainers(double weightThreshold, List<List<Container>> cargoBay)
+        {
+            return cargoBay.SelectMany(r => r).Where(x => x.Items
+                    .Sum(i => i.Weight) > weightThreshold)
+                    .Select(id =>id.ContainerID).OrderBy(a => a).ToList();
+        }
+
+        static Dictionary<string, int> GetitemCountsByCategory(List<List<Container>> cargoBay)
+        {
+            Dictionary<string, int> res = new Dictionary<string, int>();
+            foreach (var row in cargoBay)
+            {
+                foreach (var number in row)
+                {
+                    foreach (var item in number.Items)
+                    {
+                        if (res.ContainsKey(item.Category)) res[item.Category]++;
+                        else res.Add(item.Category, 1);
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        static public List<Item> FlattenAndSortShipment(List<List<Container>> cargoBay)
+        {
+            if (cargoBay == null)
+                return new List<Item>();
+            var newList = cargoBay.Where(r => r != null)
+                                  .SelectMany(r => r)
+                                  .Where(con => con != null && con.Items != null)
+                                  .SelectMany(con => con.Items)
+                                  .Where(it => it != null)
+                                  .OrderBy(it => it.Category)
+                                  .ThenByDescending(it => it.Weight)
+                                  .ToList();
+            return newList;
+        }
+        static void Main(string[] args)
+        {
+            var cargoBay = new List<List<Container>>
+            {
+                // ROW 0: High-Value Tech Rows
+                new List<Container>
+                {
+                    new Container("C001", new List<Item>
+                    {
+                        new Item("Laptop", 2.5, "Tech"),
+                        new Item("Monitor", 5.0, "Tech"),
+                        new Item("Smartphone", 0.5, "Tech")
+                    }),
+
+                    new Container("C104", new List<Item>
+                    {
+                        new Item("Server Rack", 45.0, "Tech"),
+                        new Item("Cables", 1.2, "Tech")
+                    })
+                },
+
+                // ROW 1: Mixed Consumer Goods
+                new List<Container>
+                {
+                    new Container("C002", new List<Item>
+                    {
+                        new Item("Apple", 0.2, "Food"),
+                        new Item("Banana", 0.2, "Food"),
+                        new Item("Milk", 1.0, "Food")
+                    }),
+
+                    new Container("C003", new List<Item>
+                    {
+                        new Item("Table", 15.0, "Furniture"),
+                        new Item("Chair", 7.5, "Furniture")
+                    })
+                },
+
+                // ROW 2: Fragile & Perishables
+                new List<Container>
+                {
+                    new Container("C205", new List<Item>
+                    {
+                        new Item("Vase", 3.0, "Decor"),
+                        new Item("Mirror", 12.0, "Decor")
+                    }),
+
+                    new Container("C206", new List<Item>())
+                },
+
+                // ROW 3: Empty Row
+                new List<Container>()
+            };
+
+
+            //////////////////////////////////////////////////////////////////////
+
+            Console.WriteLine("Enter the Threshold");
+            double weightThreshold = int.Parse(Console.ReadLine());
+            List<string> container = FindHeavyContainers(weightThreshold, cargoBay);
+            foreach (var containers in container)
+            {
+                Console.WriteLine("\t" + containers);
+            }
+
+            //////////////////////////////////////////////////////////////////////
+            ///
+            Console.WriteLine();
+            Dictionary<string, int> ans = GetitemCountsByCategory(cargoBay);
+            foreach (var item in ans)
+            {
+                Console.WriteLine($"{item.Key} have count of {item.Value}");
+            }
+            Console.WriteLine();
+
+            //////////////////////////////////////////////////////////////////////
+            ///
+            Console.WriteLine();
+            var flat = FlattenAndSortShipment(cargoBay);
+            foreach (var f in flat)
+            {
+                Console.WriteLine($"{f.Category} {f.Name} {f.Weight}");
+            }
+        }
+    }
+}
---
+++ Assignments/Day 50/StudentDictionary/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace StudentDictionary
+{
+    class Student
+    {
+        public int StuId { get; set; }
+        public string SName { get; set; }
+
+        public Student(int id, string name)
+        {
+            StuId = id;
+            SName = name;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Student temp = obj as Student;
+            return this.StuId == temp.StuId && this.SName == temp.SName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(StuId, SName);
+        }
+    }
+
+    class Management
+    {
+        static Dictionary<Student, int> StudRecord = new Dictionary<Student, int>();
+
+        public static void AddStud(Student s, int marks)
+        {
+            if (!StudRecord.ContainsKey(s))
+            {
+                StudRecord.Add(s, marks);
+            }
+            else if (marks > StudRecord[s])
+            {
+                StudRecord[s] = marks;
+            }
+        }
+
+        public static void Display()
+        {
+            foreach (var stud in StudRecord)
+            {
+                Console.WriteLine($"Name : {stud.Key.SName}   Marks : {stud.Value}");
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(21, "Rahul");
+            Management.AddStud(s1, 56);
+
+            Student s2 = new Student(34, "Jai");
+            Management.AddStud(s2, 80);
+
+            Student s3 = new Student(31, "Sneha");
+            Management.AddStud(s3, 90);
+
+            Student s4 = new Student(31, "Sneha");
+            Management.AddStud(s4, 100);
+
+            Management.Display();
+        }
+    }
+}
