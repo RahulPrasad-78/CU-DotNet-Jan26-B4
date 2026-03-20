33b4902518cc62c7d5bd284e8d9cdeeb382382fa
Rahul Prasad <your-email@example.com>
2026-03-20 12:14:55 +0530

    Day 62 Complete

---
+++ Assignments/Day 62/LoanManagementWebAPI/Models/Loan.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoanManagementWebAPI.Models
+{
+    public class Loan
+    {
+        public int LoanId { get; set; }
+        
+        [Required]
+        public string BorrowerName { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public int LoanTermMonths { get; set; }
+
+        [Required]
+        public bool isApproved { get; set; }
+    }
+}
