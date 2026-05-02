bdf2ce9b76139b86c46141b7f4bb5a1eb34c6366
Rahul Prasad <your-email@example.com>
2026-05-02 10:41:36 +0530

    week 17 completed

---
+++ Assessments/Week 17/The_Horizon/services/identity-service/Migrations/20260430234100_addedroles.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LearningPlatformAuth.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedroles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ApprovalDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApprovalNotes",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsApproved",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovalDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ApprovalNotes",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IsApproved",
+                table: "AspNetUsers");
+        }
+    }
+}
